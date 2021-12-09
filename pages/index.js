bb881fea302dc4a544b421013591cf597ce91193
Maamoun <grissa.maamoun@gmail.com>
2021-12-09 12:37:03 +0100

    Comming Soon2

--- pages/index.js
+++ pages/index.js
@@ -10,25 +10,27 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <div className="page-container">
-        <div className="flex">
-            <div className="col">
-                <div className="infos-container">
-                    <img src="/images/logo.png" alt="Veilo-Logo" />
-                    <h2>Comming soon ...</h2>
-                    <div className="socials">
-                        <a href="https://www.facebook.com/Veilo-108764824974314/"><img src="/images/facebook.png" alt="Facebook-Logo" /></a>
-                        <a href="https://www.instagram.com/veilo.brand/"><img src="/images/instagram.png" alt="Instagram-Logo" /></a>
-                    </div>
-                </div>
-            </div>
-            <div className="col">
-                <div className="img-container">
-                    <img src="/images/cover.jpg" alt="Veilo" />
-                </div>
-            </div>
+      <div className='container'>
+        <div className="page-container">
+          <div className="flex">
+              <div className="col">
+                  <div className="infos-container">
+                      <img src="/images/logo.png" alt="Veilo-Logo" />
+                      <h2>Comming soon ...</h2>
+                      <div className="socials">
+                          <a href="https://www.facebook.com/Veilo-108764824974314/"><img src="/images/facebook.png" alt="Facebook-Logo" /></a>
+                          <a href="https://www.instagram.com/veilo.brand/"><img src="/images/instagram.png" alt="Instagram-Logo" /></a>
+                      </div>
+                  </div>
+              </div>
+              <div className="col">
+                  <div className="img-container">
+                      <img src="/images/cover.jpg" alt="Veilo" />
+                  </div>
+              </div>
+          </div>
         </div>
-    </div>
+      </div>
     </div>
   )
 }
